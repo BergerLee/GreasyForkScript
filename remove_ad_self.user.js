c623c33187082468826073013738dc58828660cc
Berger <bergerleee@yahoo.com>
2024-08-11 10:55:50 +0800

    [新增]适配

--- remove_ad_self.user.js
+++ remove_ad_self.user.js
@@ -7,6 +7,7 @@
 // @match       https://www.applnn.cc/*
 // @match       https://*.lanzout.com/
 // @match       https://*.lanzout.com/*
+// @match       https://m.775sy.com/*
 // @grant       unsafeWindow
 // @grant       GM_addStyle
 // @run-at      document-start
@@ -68,6 +69,54 @@
         console.log(downloadBottomAd)
     }
 
+    // 提前下载游戏
+    function nw_game_previous_download(){
+        const observer = new MutationObserver(function(mutationsList, observer) {
+            const download_box = document.querySelector('.three_btn_box');
+            if (download_box) {
+                if (download_box.children.length === 0){
+                    download_box.innerHTML=
+                        `
+                        <uni-view data-v-6267e90e="" class="three_btn_box__item down" style="background: rgb(1, 143, 255);">
+                        <uni-view>提前下载（插件）</uni-view>
+                        </uni-view>
+                        `
+                }
+
+                const downloadButton = download_box.querySelector('.three_btn_box__item.down');
+                if (downloadButton) {
+                    downloadButton.addEventListener('click', function() {
+                        const idMatch = url.match(/[?&]id=([0-9]+)/)
+                        if (idMatch && idMatch[1]) {
+                            window.location.href = `https://oss.775sy.com/android/game_package${idMatch[1]}.apk`
+                        } else {
+                            console.log('未找到 id 参数');
+                            return null;
+                        }
+                    });
+                }
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
+
+    function nw_game_ad_normal(){
+        const observer = new MutationObserver(function(mutationsList, observer) {
+            const tabbarItemArrays = document.querySelectorAll('.u-tabbar__content__item-wrapper > .u-tabbar-item');
+
+            if (tabbarItemArrays.length > 0) {
+                utils.removeElement(tabbarItemArrays[2])
+                utils.removeElement(tabbarItemArrays[3])
+                observer.disconnect();
+            }
+
+
+        });
+
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
 
     let main = {
         initNormal() {
@@ -75,6 +124,9 @@
                 app_lnn_AD_normal()
             }else if (url.indexOf('lanzout.com') !== -1){
                 lan_z_out_AD_normal()
+            }else if (url.indexOf('775sy.com') !== -1){
+                nw_game_previous_download()
+                nw_game_ad_normal()
             }
         },
 
